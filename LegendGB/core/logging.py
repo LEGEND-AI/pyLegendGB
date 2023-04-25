56e324aa856178d5bee4dca9a4c95dcda9ed25fa
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-25 15:32:10 +0530

    Create logging.py

---
+++ LegendGB/core/logging.py
@@ -0,0 +1,14 @@
+import logging
+
+
+logging.basicConfig(
+    format="[%(asctime)s]:[%(levelname)s]:[%(name)s]:: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+)
+
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+LOGS = logging.getLogger("Legend-Group-Bot")
